8a8fbf1667501d258b32bdb3eaa965c3e56684fe
Nthenge <abrahamnetsec@gmail.com>
2025-04-17 01:53:05 +0300

    added appjs file which is express app to handle routing and the sever, added buttons in about and index pages

--- App.js
+++ App.js
@@ -3,5 +3,25 @@ const express = require('express')
 const app = express();
 
 //listen for requests
+app.listen(3000);
 
-app.listen(3000)
+app.get('/',(req, res) => {
+    //res.send('<p>Home page</p>')
+    res.sendFile('./views/index.html', {root: __dirname})
+})
+
+app.get('/about',(req, res) => {
+    //res.send('<p>About page</p>')
+    res.sendFile('./views/about.html', {root: __dirname})
+})
+
+
+//redirectss
+app.get('/about-us', (req, res)=>{
+    res.redirect('/about');
+})
+
+//404 page
+app.use((req, res) => {
+    res.status(404).res.sendFile('./views/404.html', {root: __dirname})
+})  
