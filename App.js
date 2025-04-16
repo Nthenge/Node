3114dd730b0e22437b4ff1a88dda76763e0de9dd
Nthenge <abrahamnetsec@gmail.com>
2025-04-17 02:24:14 +0300

    Added ejs to work with views

--- App.js
+++ App.js
@@ -1,27 +1,28 @@
 const express = require('express')
-
 const app = express();
+// register view  engine
+app.set('view engine', 'ejs');
+
+
 
 //listen for requests
 app.listen(3000);
 
 app.get('/',(req, res) => {
     //res.send('<p>Home page</p>')
-    res.sendFile('./views/index.html', {root: __dirname})
+    res.render('index');
 })
 
 app.get('/about',(req, res) => {
     //res.send('<p>About page</p>')
-    res.sendFile('./views/about.html', {root: __dirname})
+    res.render('about');
 })
 
-
-//redirectss
-app.get('/about-us', (req, res)=>{
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create')
 })
 
 //404 page
 app.use((req, res) => {
-    res.status(404).res.sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404')
 })  
